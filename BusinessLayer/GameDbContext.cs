864fac8503a71031a23cce94ccab2a8e3f224765
Revold <ivan_mladenov2004@abv.bg>
2022-05-27 09:16:23 +0300

    idk

---
+++ BusinessLayer/GameDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=IVAN-VIPER\SQLEXPRESS;;Trusted_Connection=True;");
+            }
+        }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+
+
+    }
+}
